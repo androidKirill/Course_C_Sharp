112b65bd2071802ce1706845cc85f344a04065fd
celestialmary <prime_proger@mail.ru>
2022-07-08 11:33:35 +0300

    Task 1_2

---
+++ 1_Lesson/1_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// 1. Напишите программу, которая на вход принимает два числа и проверяет,
+//    является ли первое число квадратом второго.
+
+Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write a number b: ");
+int b = int.Parse(Console.ReadLine());
+
+if (b == a * a)
+{
+    Console.WriteLine("Yes");    
+}
+else
+{
+    Console.WriteLine("No");
+}
