e8a06b5ae0704d62cf3dfc9f1d9db01037a9d864
celestialmary <prime_proger@mail.ru>
2022-07-12 09:49:19 +0300

    HW 1_5

---
+++ 1_Lesson/HW/HW_1_5/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает последнюю цифру этого числа.
+
+Console.WriteLine("Enter a three-digit number");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(num % 10);
